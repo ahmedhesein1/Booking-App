b2cf123f42ee32e9eba70ed753f595abc117beae
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-09-20 19:02:49 +0300

    Refactor Routes To be More cleaner

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,11 +2,17 @@ import express from "express";
 import * as userController from "../controllers/userController.js";
 import * as authController from "../controllers/authController.js";
 const router = express.Router();
+router
+  .route("/")
+  .get(
+    authController.protect,
+    authController.restrictTo("admin"),
+    userController.getAllUsers
+  );
 router.use(authController.protect);
-router.route("/").get(authController.restrictTo("admin"),userController.getAllUsers);
 router
   .route("/:id")
   .get(userController.getUser)
-  .patch(authController.protect,authController.restrictTo("admin"),userController.updateUser)
-  .delete(authController.protect,authController.restrictTo("admin"),userController.deleteUser);
+  .patch(authController.restrictTo("admin"), userController.updateUser)
+  .delete(authController.restrictTo("admin"), userController.deleteUser);
 export default router;
